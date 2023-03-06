53cbef0acc1afd863b0fd7ec9a33e5f2e4a5933d
Joshua <joshtylero98@gmail.com>
2023-03-05 21:03:41 -0500

    Pushing for minus test

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,10 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	public int division(int a, int b) {
+		return a / b;
+	}
 }
