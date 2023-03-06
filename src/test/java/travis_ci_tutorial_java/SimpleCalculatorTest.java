0cdb31c19ae7ad3ce3e59aa0a44beb537f1b6bf7
jouypo23 <32280658+jouypo23@users.noreply.github.com>
2023-03-05 22:08:41 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -14,6 +14,6 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testMinus() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.minus(5, 3), 2);
+		assertEquals(calc.minus(6, 3), 3);
 	}
 }
